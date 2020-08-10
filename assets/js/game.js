5c763df7343b73763dc3dad7fc7fe94d8da936fa
greyw <greywhitt@gmail.com>
2020-08-09 20:02:30 -0500

    added basic functions game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("what is you robots name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+
+console.log(10 + 10);
+
+console.log("Our robots name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
